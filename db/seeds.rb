9962962e9acfabdef37461bebd4d1abefa29187b
nataliavega <nvegar@gmail.com>
2023-08-29 10:30:12 +0200

    added model with scaffold, added datepicker, toggle effect with stimulus and insert in list with ajax

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Monument.create(name: "Eiffel tower", address: "Champ de Mars, 5 Av. Anatole France, 75007 Paris", opening_date: "1889/03/31")
+Monument.create(name: "Statue of Liberty", address: "New York, État de New York 10004, États-Unis", opening_date: "1886/10/28")
